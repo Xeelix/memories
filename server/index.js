574c0885e0143a7b7ce0fe9cf4cdc42740cb5fdb
xeelix <fritlangoof@gmail.com>
2021-11-01 18:25:42 +0700

    Intitial Commit

---
+++ server/index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+app.get('/', (req, res) => {
+    res.send("Working...");
+})
+
+app.use(express.json({ limit: '30MB', extended: true }));
+app.use(express.urlencoded({ limit: '30MB', extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port: ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(e.message);
+    })
